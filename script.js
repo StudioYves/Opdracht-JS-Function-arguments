47d1e38a0f0f83f659e9014b20cd05f9297efc92
Ivo Kersten <82444976+StudioYves@users.noreply.github.com>
2021-06-04 13:14:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//console.log("The wall has been painted red");
+
+const wallPainting = function(color, direction){
+    console.log("The " + direction + " wall has been painted " + color);
+};
+
+wallPainting("green");
+
+wallPainting("blue");
+
+wallPainting("orange", "north");
+
+
+
+
+
+
+
+
+
+
+
+
